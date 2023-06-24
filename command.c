95172442f202d50f1f6ea60127419ddf518eae1f
lasyadevops <bommalasya29@gmail.com>
2023-06-24 11:59:54 +0530

    this is my second commit

---
+++ command.c
@@ -0,0 +1,21 @@
+git clone(it is a command to clone remode repository into local machine)
+cd(change the directory)
+ll(to see the files and directories in long list)
+ls(to see in the short list of directories and files ,only names)
+ll -a(to see the long list of files including hidden files)
+git status(to see the status of files)
+git add(is a command to add files in stagging or 1index)
+git add(is a command to add files in stagging or 1index)
+git commit -m "message" filename
+git commit -m "message"(to send all files)
+git add *(used to add all the files in staging or index)
+git add file name filename(to add two file simultaneously)	
+git add filename filename ..upto nth file name
+git branch(to whether the file is in main or not)
+git reset filename(change the file from staging to working directory or commited files to working directory)
+git stage file name(to stage only file)
+git stage filename filename(to stage two files simultaneously)
+git stage *(add all the at a time)	
+git global information track
+git is meant for source code management
+
